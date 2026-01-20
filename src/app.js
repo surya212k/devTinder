7a696a6c55eef7450de0b321e32b287c18013853
surya212k <surya212k@gmail.com>
2026-01-20 15:22:33 +0530

    regex

--- src/app.js
+++ src/app.js
@@ -3,34 +3,11 @@ const express = require('express');
 
 const app = express()
 
-app.get("/user", (req, res) =>{
+app.get("/user/:userId/:password", (req, res) =>{
+    console.log(req.params)
     res.send({firstname: "Ajithkumar", lastname:"Singh"})
 })
 
-app.post("/user", (req,res)=>{
-    res.send("Data sent to db successfully")
-})
-
-app.put("/user", (req,res) =>{
-    res.send("Data put successfully")
-})
-
-app.patch("/user", (req, res) =>{
-    res.send("Data patch successfull")
-})
-
-app.delete("/user", (req,res) =>{
-    res.send("Data deleted successfully")
-})
-
-app.use("/user", (req, res)=>{
-    res.send("Hello from user")
-})
-
-
-app.use("/test", (req, res)=>{
-    res.send("Hello from test")
-})
 
 app.listen(3000, () =>{
     console.log('Server is running on port 3000');
