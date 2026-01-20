5beb7edee3cf6040ee93c94115eed4d7eedb9058
surya212k <surya212k@gmail.com>
2026-01-20 23:25:24 +0530

    Multiple Route handlers

--- src/app.js
+++ src/app.js
@@ -3,11 +3,25 @@ const express = require('express');
 
 const app = express()
 
-app.get("/user/:userId/:password", (req, res) =>{
-    console.log(req.params)
-    res.send({firstname: "Ajithkumar", lastname:"Singh"})
-})
-
+app.use("/user", (req, res, next) =>{
+    console.log("Route handler 1 called");
+    next();
+},
+(req, res, next) =>{
+    console.log("Route handler 2 called");
+    next();
+},
+(req, res, next) =>{
+    console.log("Route handler 3 called");
+    next()
+},
+(req, res, next) =>{
+    console.log("Route handler 4 called");
+    next();
+},
+(req, res) =>{
+    res.send("Final response from /user route");
+});
 
 app.listen(3000, () =>{
     console.log('Server is running on port 3000');
